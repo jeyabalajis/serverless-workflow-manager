d80703357b7b9177085f2c287bc8493d4fa15b12
Jeya <jeyabalaji.subramanian@gmail.com>
2021-07-24 14:38:25 +0530

    terraform configuration added

---
+++ terraform_setup/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    organization = "jeyasandbox"
+    workspaces {
+      name = "serverless-workflow-manager"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+resource "aws_ecr_repository" "demo-app-repository" {
+  name = var.aws_resource_prefix
+}
